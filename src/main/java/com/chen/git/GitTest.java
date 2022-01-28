82670174a2103e6ee28a4a5686f4dd508334055d
chen <youyizhongyun@gmail.com>
2022-01-28 11:06:58 +0800

    first commit

---
+++ src/main/java/com/chen/git/GitTest.java
@@ -0,0 +1,16 @@
+package com.chen.git;
+
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+import org.w3c.dom.ls.LSOutput;
+
+/**
+ * @author Chen ZhiXin
+ * @create 2022-01-28 11:02
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+
+
+    }
+}
